5ff2240fb2364ffa2e71e5bd77d3cba3adcbd8f1
Bo Zhang <bo@gradle.com>
2019-06-08 19:48:40 +0800

    Update repo

--- src/test/java/com/github/hcsp/calculation/MainTest.java
+++ src/test/java/com/github/hcsp/calculation/MainTest.java
@@ -8,7 +8,7 @@ class MainTest {
     @Test
     public void test() {
         assertEquals("非法输入", Main.formatStudentScore("", -1));
-        assertEquals("非法输入", Main.formatStudentScore("", 100));
+        assertEquals("非法输入", Main.formatStudentScore("", 101));
 
         int score1 = new java.util.Random().nextInt(59);
         assertEquals("学生张三的分数是" + score1 + "，评级是C", Main.formatStudentScore("张三", score1));
@@ -17,6 +17,6 @@ class MainTest {
         assertEquals("学生张三的分数是" + score2 + "，评级是B", Main.formatStudentScore("张三", score2));
 
         int score3 = new java.util.Random().nextInt(10) + 90;
-        assertEquals("学生张三的分数是" + score3 + "，评级是C", Main.formatStudentScore("张三", score3));
+        assertEquals("学生张三的分数是" + score3 + "，评级是A", Main.formatStudentScore("张三", score3));
     }
 }
