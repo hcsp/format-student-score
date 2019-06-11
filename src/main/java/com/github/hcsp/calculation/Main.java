abc21c5a1a74aedf02c9931beb2900a329a972d4
JayChenFE <chenjiehwy@163.com>
2019-06-11 15:28:45 +0800

    对学生的分数进行评级

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -12,10 +12,26 @@ public class Main {
      * <p>score为其他值时，返回"非法输入"。
      *
      * @param student 学生的姓名
-     * @param score 学生的分数
+     * @param score   学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        double[] range = {60, 90, 100};
+        String[] grade = {"C", "B", "A"};
+
+        if (score < 0 || score > 100) {
+            return "非法输入";
+        }
+        int gradeLevel = 0;
+        String studentGrade = grade[0];
+        while (score >= range[gradeLevel++]) {
+            studentGrade = grade[gradeLevel];
+        }
+
+        String result = String.format("学生%s的分数是%d，评级是%s", student, score, studentGrade);
+        return result;
+
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
