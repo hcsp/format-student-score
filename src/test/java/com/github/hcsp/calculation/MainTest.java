67dbe832568d14f1e15544806727abac02217590
Bo Zhang <bo@gradle.com>
2019-06-15 19:25:11 +0800

    Update repo

--- src/test/java/com/github/hcsp/calculation/MainTest.java
+++ src/test/java/com/github/hcsp/calculation/MainTest.java
@@ -1,5 +1,6 @@
 package com.github.hcsp.calculation;
 
+import java.util.Random;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,15 +10,15 @@ public class MainTest {
         Assertions.assertEquals("非法输入", Main.formatStudentScore("", -1));
         Assertions.assertEquals("非法输入", Main.formatStudentScore("", 101));
 
-        int score1 = new java.util.Random().nextInt(59);
+        int score1 = new Random().nextInt(59);
         Assertions.assertEquals(
                 "学生张三的分数是" + score1 + "，评级是C", Main.formatStudentScore("张三", score1));
 
-        int score2 = new java.util.Random().nextInt(10) + 60;
+        int score2 = new Random().nextInt(10) + 60;
         Assertions.assertEquals(
                 "学生张三的分数是" + score2 + "，评级是B", Main.formatStudentScore("张三", score2));
 
-        int score3 = new java.util.Random().nextInt(10) + 90;
+        int score3 = new Random().nextInt(10) + 90;
         Assertions.assertEquals(
                 "学生张三的分数是" + score3 + "，评级是A", Main.formatStudentScore("张三", score3));
     }
