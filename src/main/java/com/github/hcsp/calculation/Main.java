e6db56229bffa5cd864f246346d1157cc28f1d04
zzxx9426 <97177440@qq.com>
2019-06-28 17:02:58 +0800

    string +

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -11,10 +11,10 @@ public class Main {
      * <p>score为其他值时，返回"非法输入"。
      *  @param student 学生的姓名
      * @param score 学生的分数
-     * @return
+     * @return 结果字符串
      */
     public static String formatStudentScore(String student, int score) {
-        String str = "学生" + student + "的分数是" + score + ",评级是";
+        String str = "学生" + student + "的分数是" + score + "，评级是";
          if(score >= 90 && score <= 100){
              return str + "A";
          }if(score >= 60 && score <= 89){
