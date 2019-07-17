b15404f1be9a7b7a7d48d2d0659018b9beb1389c
胡凯波 <1069977596@qq.com>
2019-07-17 21:08:11 +0800

    使用位运算进行乘除运算

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -16,13 +16,13 @@ public class Main {
      */
     public static String formatStudentScore(String student, int score) {
         if (90<=score&&score<=100){
-            return "学生"+student+"的分数是"+score+",评级是A";
+            return "学生"+student+"的分数是"+String.valueOf(score)+",评级是A";
         }
         else if (60<=score&&score<=89){
-            return "学生"+student+"的分数是"+score+",评级是B";
+            return "学生"+student+"的分数是"+String.valueOf(score)+",评级是B";
         }
         else if (0<=score&&score<=59){
-            return "学生"+student+"的分数是"+score+",评级是B";
+            return "学生"+student+"的分数是"+String.valueOf(score)+",评级是B";
         }
         return "非法输入";
     }
