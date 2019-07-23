d784c880f53ba0ea228bb9b4d35c25beab5ffc5b
Bigbig Zheng <39644918+zhengbigbig@users.noreply.github.com>
2019-07-23 10:16:11 +0800

    区别中英文逗号 (#38)
    区别中英文逗号 (#38)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -14,7 +14,19 @@ public class Main {
      * @param score 学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        String result;
+        if(score <= 100 && score >=90){
+            result = "学生" + student + "的分数是" + score+"，评级是A";
+        }else if(score>=60 && score<=89){
+            result = "学生" + student + "的分数是" + score+"，评级是B";
+        }else if(score>=0 && score<=59){
+            result = "学生" + student + "的分数是" + score+"，评级是C";
+        }else {
+            result = "非法输入";
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
