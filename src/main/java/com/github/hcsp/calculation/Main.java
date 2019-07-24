8fb9a4665aace95012d917c5e878bf830e04c740
Ben <50296011+MaoYuanFang@users.noreply.github.com>
2019-07-24 17:31:23 +0800

    Update Main.java

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -14,7 +14,20 @@ public class Main {
      * @param score 学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        return "学生" + student + "的分数是" + score + ",评级是" + grade(score); 
+    }
+    
+    private static String grade(int score){
+        if(90 <= score && score <= 100){
+            return "A";
+        }else if(60 <= score && score <= 89){
+            return "B";
+        }else if(0 <= score && score <= 59){
+            return "C";
+        }
+        return "非法输入";
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
