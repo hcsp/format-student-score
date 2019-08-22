55bddd4fa7e93bf29b8a3b9ce628ec79c749542f
Bo Zhang <zhangbodut@gmail.com>
2019-08-22 15:39:26 +0800

    Support all commas

--- src/test/java/com/github/hcsp/calculation/MainTest.java
+++ src/test/java/com/github/hcsp/calculation/MainTest.java
@@ -1,25 +1,21 @@
-package com.github.hcsp.calculation;
 
-import java.util.Random;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+package com.github.hcsp.calculation;
+import org.junit.jupiter.api.Test;;import java.util.Random;;import org.junit.jupiter.api.Assertions;
 
 public class MainTest {
-    @Test
-    public void test() {
-        Assertions.assertEquals("非法输入", Main.formatStudentScore("", -1));
-        Assertions.assertEquals("非法输入", Main.formatStudentScore("", 101));
+@Test
+public void test() {
+  Assertions.assertEquals("非法输入", Main.formatStudentScore("", -1));
+  Assertions.assertEquals("非法输入", Main.formatStudentScore("", 101));
 
-        int score1 = new Random().nextInt(59);
-        Assertions.assertEquals(
-                "学生张三的分数是" + score1 + "，评级是C", Main.formatStudentScore("张三", score1));
+  int score1 = new Random().nextInt(59);
+  Assertions.assertEquals("学生张三的分数是" + score1 + "评级是C", Main.formatStudentScore("张三", score1).replace(',', '').replace('，', ''));
 
-        int score2 = new Random().nextInt(10) + 60;
-        Assertions.assertEquals(
-                "学生张三的分数是" + score2 + "，评级是B", Main.formatStudentScore("张三", score2));
+  int score2 = new Random().nextInt(10) +60;
+  Assertions.assertEquals("学生张三的分数是" + score2 + "评级是B", Main.formatStudentScore("张三", score2).replace(',', '').replace('，', ''));
 
-        int score3 = new Random().nextInt(10) + 90;
-        Assertions.assertEquals(
-                "学生张三的分数是" + score3 + "，评级是A", Main.formatStudentScore("张三", score3));
-    }
+  int score3 = new Random().nextInt(10) +90;
+  Assertions.assertEquals("学生张三的分数是" + score3 + "评级是A", Main.formatStudentScore("张三", score3).replace(',', '').replace('，', ''));
+}
+
 }
