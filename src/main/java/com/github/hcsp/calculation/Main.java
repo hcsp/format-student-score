9167856ba549e1c614ac6aca8fc65a10da302116
kungua <52815012+kungua@users.noreply.github.com>
2019-08-24 09:08:29 +0800

    finish function formatStudentScore (#56)
    finish function formatStudentScore (#56)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -14,7 +14,19 @@ public class Main {
      * @param score 学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        if (score >= 0 && score <= 100) {
+            String level = "A";
+            if (score <= 59) {
+                level = "C";
+            } else if (score <= 89) {
+                level = "B";
+            }
+            return "学生" + student + "的分数是" + score + "，评级是" + level;
+        } else {
+            return "非法输入";
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
