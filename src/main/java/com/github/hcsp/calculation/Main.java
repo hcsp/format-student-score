e5692b29e49042858b9c80d8c50c3c09b5771a00
Your Name <you@example.com>
2019-10-12 11:39:19 +0800

    fix

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -16,8 +16,8 @@ public class Main {
      */
     public static String formatStudentScore(String student, int score) {
         if (90<=score && score<=100) return "学生" + student +"的分数是" +score+"评级是A";
-        if (60<=score && score<=89)  return "学生" + student +"的分数是" +score+"评级是A";
-        if (0<=score && score<=59)   return "学生" + student +"的分数是" +score+"评级是A";
+        if (60<=score && score<=89)  return "学生" + student +"的分数是" +score+"评级是B";
+        if (0<=score && score<=59)   return "学生" + student +"的分数是" +score+"评级是C";
         return "非法输入";
     }
 
