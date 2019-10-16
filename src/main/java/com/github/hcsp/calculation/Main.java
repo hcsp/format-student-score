f104ff2bb89c9e6e3d60ab3e7134cabc638b2dc7
xuepan-summer <54898607+xuepan-summer@users.noreply.github.com>
2019-10-16 17:14:17 +0800

    String加法 (#81)
    String加法 (#81)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -11,10 +11,22 @@ public class Main {
      * <p>score为其他值时，返回"非法输入"。
      *
      * @param student 学生的姓名
-     * @param score 学生的分数
+     * @param score   学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        if (score >= 0 && score <= 100){
+            if(score <= 59){
+                return "学生"+student+"的分数是"+score+"，评级是C";
+            }
+            if(score <= 89){
+                return "学生"+student+"的分数是"+score+"，评级是B";
+            }
+            return "学生"+student+"的分数是"+score+"，评级是A";
+        }else{
+            return "非法输入";
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
