8d96e7bd529bc70145a3e7fb3edd7bbbe4949f4c
linsinan1995 <47880367+linsinan1995@users.noreply.github.com>
2019-10-19 01:10:15 +0200

    format-student-score (#83)
    format-student-score (#83)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -14,7 +14,17 @@ public class Main {
      * @param score 学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        if (score < 0 || score > 100) {
+            return "非法输入";
+        } else if (score < 60) {
+            return "学生"+ student +"的分数是" + score + "，评级是C";
+        } else if (score < 90) {
+            return "学生"+ student +"的分数是" + score + "，评级是B";
+        } else{
+            return "学生"+ student +"的分数是" + score + "，评级是A";
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
