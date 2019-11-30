c73142f15011dacb829959a474667c7f0a471aed
sunkuangdong <769296817@qq.com>
2019-11-30 23:01:40 +0800

    完成学生评级

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -14,7 +14,19 @@ public class Main {
      * @param score 学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        String s = "学生" + student + "的分数是" + score + "，评级是";
+        if (score >= 90 && score <= 100) {
+            s += "A";
+        } else if (score >= 60 && score <= 89) {
+            s += "B";
+        } else if (score >= 0 && score <= 59) {
+            s += "C";
+        } else {
+            s = "非法输入";
+        };
+        return s;
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
