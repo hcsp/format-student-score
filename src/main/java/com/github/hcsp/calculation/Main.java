a2ee2c9472f21c02ddbd65a20eec3677b11fa78e
Bing Qin <qinbing08@gmail.com>
2020-01-01 21:51:56 +0800

    练习String的加法操作

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -22,9 +22,7 @@ public class Main {
             str = "B";
         } else if (score >= 0 && score <= 59) {
             str = "C";
-        } else {
-            str = "非法输入";
-        }
+        } else return "非法输入";
         return "学生" + student + "的分数是" + score + ",评级是" + str;
     }
 
