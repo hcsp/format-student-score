f85a72d75cfdb09cf2047375c17b5c0dfab502b6
Blink1024 <blink1024@foxmail.com>
2020-01-06 01:57:50 +0800

    对学生的分数进行评级

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -11,10 +11,22 @@ public class Main {
      * <p>score为其他值时，返回"非法输入"。
      *
      * @param student 学生的姓名
-     * @param score 学生的分数
+     * @param score   学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        if (score < 0 || score > 100) {
+            return "非法输入";
+        } else {
+            if (score < 60) {
+                return "学生" + student + "的分数是" + score + "，评级是C";
+            } else {
+                return score < 90 ? "学生" + student + "的分数是" + score + "，评级是B" : "学生" + student + "的分数是" + score + "，评级是A";
+            }
+        }
+
+
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
