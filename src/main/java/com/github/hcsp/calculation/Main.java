8773cf1eb3af020e624b5bd8efc48d88967a110b
zml59 <wan624711573@163.com>
2020-01-07 23:37:46 +0800

    评估学生考试分数。

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -14,7 +14,15 @@ public class Main {
      * @param score 学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+
+        if (score >= 0 & score <= 89) {
+            if (score <= 59) {
+                return "学生" + student + "的分数是" + score + "，评级是C";
+            } else return "学生" + student + "的分数是" + score + "，评级是B";
+        } else return "非法输入";
+
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
