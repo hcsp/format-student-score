18af50b0a79a3483865f6b2048eac0d8ffdc1acd
chenxiaolani <2524729719@qq.com>
2020-01-13 16:42:42 +0800

    条件判断

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -11,10 +11,22 @@ public class Main {
      * <p>score为其他值时，返回"非法输入"。
      *
      * @param student 学生的姓名
-     * @param score 学生的分数
+     * @param score   学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        String backStr;
+        if (90 <= score && score <= 100) {
+            backStr = "学生" + student + "的分数是" + score + ",评级是A";
+        } else if (60 <= score && score <= 89) {
+            backStr = "学生" + student + "的分数是" + score + ",评级是B";
+        } else if (0 <= score && score <= 59) {
+            backStr = "学生" + student + "的分数是" + score + ",评级是C";
+        } else {
+            backStr = "非法输入";
+        }
+        return backStr;
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
