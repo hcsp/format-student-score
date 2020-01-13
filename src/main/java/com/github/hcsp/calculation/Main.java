0a64fe549811d1c85b4e9c965a872ec893ec274d
wenzhe <mysteryven@gmail.com>
2020-01-13 23:40:11 +0800

    if else

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -14,7 +14,20 @@ public class Main {
      * @param score 学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        String x = "";
+        if (score >= 90 && score <= 100) {
+            x = "A";
+        } else if (score >= 60 && score <= 89) {
+            x = "B";
+        } else if (score >= 0 && score <= 59) {
+            x = "C";
+        } else {
+            return "非法输入";
+        }
+
+        return "学生" + student + "的分数是" + score+ "，" + "评级是" + x;
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
