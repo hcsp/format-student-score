d7b0941326effad13c0663d49b37d0ca0656d0af
FS <553931681@qq.com>
2020-02-10 23:42:07 +0800

    String的加法操作，并修改了实现方法

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -17,38 +17,17 @@ public class Main {
      * @return 结果字符串
      */
     public static String formatStudentScore(String student, int score) {
-//        if (score > 100 || score < 0) {
-//            return "非法输入";
-//        }
+        if (score > 100 || score < 0) {
+            return "非法输入";
+        }
 
         String level;
-
-        switch (score / 10) {
-            case 10:
-                if (score % 10 > 0) {
-                    return "非法输入";
-                }
-            case 9:
-                level = "A";
-                break;
-            case 8:
-            case 7:
-            case 6:
-                level = "B";
-                break;
-            case 5:
-            case 4:
-            case 3:
-            case 2:
-            case 1:
-            case 0:
-                if (score % 10 < 0) {
-                    return "非法输入";
-                }
-                level = "C";
-                break;
-            default:
-                return "非法输入";
+        if (score >= 90) {
+            level = "A";
+        } else if (score >=60) {
+            level = "B";
+        } else {
+            level = "C";
         }
         return "学生" + student + "的分数是" + score + "，评级是" + level;
     }
