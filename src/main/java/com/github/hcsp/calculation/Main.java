51df3a61092552d78315833707ec65c068c92d6f
Tom Haiyi Wang <tomgoprox@gmail.com>
2020-03-08 17:13:24 +0800

    Implement formatStudentScore method

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -14,7 +14,18 @@ public class Main {
      * @param score 学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        String s = "学生" + student + "的分数是" + score;
+        if (score >= 90 && score <= 100) {
+            return s +"，评级是A";
+        } else if (score >= 60 && score <= 89) {
+            return s + "，评级是B";
+        } else if (score >= 0 && score <= 59) {
+            return s + "，评级是C";
+        } else {
+            return "非法输入";
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
