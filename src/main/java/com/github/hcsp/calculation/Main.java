2c010abfb20327c35eaaa8c7767b11e7748e5a6c
decolo <decolo.com>
2020-04-11 15:11:22 +0800

    done

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -14,7 +14,17 @@ public class Main {
      * @param score 学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        if (score < 0 || score > 100) {
+            return "非法输入";
+        } else if (score >= 0 && score <= 59) {
+            return "学生" + student + "的分数是" + score + "，评级是C";
+        } else if (score <= 89) {
+            return "学生" + student + "的分数是" + score + "，评级是B";
+        } else {
+            return "学生" + student + "的分数是" + score + "，评级是A";
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
