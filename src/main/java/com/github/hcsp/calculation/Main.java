4b9fc4cd0dc4ec65d60cf013b985c287ac1b48df
zhangzh <zhangzh@cnlemon.net>
2020-04-17 00:33:36 +0800

    feat: 完成挑战

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -11,10 +11,25 @@ public class Main {
      * <p>score为其他值时，返回"非法输入"。
      *
      * @param student 学生的姓名
-     * @param score 学生的分数
+     * @param score   学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        String level = null;
+        if (score >= 90 && score <= 100) {
+            level = "A";
+        }
+        if (score >= 60 && score <= 89) {
+            level = "B";
+        }
+        if (score >= 0 && score <= 59) {
+            level = "C";
+        }
+        if (level != null) {
+            return "学生" + student + "的分数是" + score + "，评级是" + level;
+        }
+        return "非法输入";
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
