18b22641174447edaad1270a6bcfa7a8de7bf75c
BenHao Huang <33121651+Lazzben@users.noreply.github.com>
2020-05-27 15:11:00 +0800

    练习String的加法操作 (#192)
    练习String的加法操作 (#192)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -11,10 +11,20 @@ public class Main {
      * <p>score为其他值时，返回"非法输入"。
      *
      * @param student 学生的姓名
-     * @param score 学生的分数
+     * @param score   学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        if (90 <= score && score <= 100) {
+            return new String("学生" + student + "的分数是" + score + "，评级是A");
+        } else if (60 <= score && score <= 89) {
+            return new String("学生" + student + "的分数是" + score + "，评级是B");
+        } else if (0 <= score && score <= 59) {
+            return new String("学生" + student + "的分数是" + score + "，评级是C");
+        } else {
+            return new String("非法输入");
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
