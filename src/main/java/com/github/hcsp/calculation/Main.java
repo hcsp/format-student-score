8b95a0448f7f02617f46300a59a02dd1fcac8088
godvvvvvv <lgj88508@163.com>
2020-06-25 15:57:37 +0800

    学生评级

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -16,12 +16,12 @@ public class Main {
      */
     public static String formatStudentScore(String student, int score) {
         if (score >= 90 && score <= 100) {
-            return "学生的分数是" + score + "，评级是A";
+            return "学生" + student + "的分数是" + score + "，评级是A";
 
         } else if (score >= 60 && score <= 89) {
-            return "学生的分数是" + score + "，评级是B";
+            return "学生" + student + "的分数是" + score + "，评级是B";
         } else if (score >= 0 && score <= 59) {
-            return "学生的分数是" + score + "，评级是C";
+            return "学生" + student + "的分数是" + score + "，评级是C";
         } else {
             return "非法输入";
         }
