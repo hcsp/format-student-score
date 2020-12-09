bdc39d26c6891c5da5492e84ff25ec33ce618a64
iRuby <1479867360@qq.com>
2020-12-09 17:08:24 +0800

    完成formatStudentScore方法

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -11,10 +11,27 @@ public class Main {
      * <p>score为其他值时，返回"非法输入"。
      *
      * @param student 学生的姓名
-     * @param score 学生的分数
+     * @param score   学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        String msg;
+        String level;
+        if (score > 100 || score < 0) {
+            msg = "非法输入";
+            return msg;
+        }
+        if (score >= 90) {
+            level = "A";
+        } else if (score >= 60) {
+            level = "B";
+        } else {
+            level = "C";
+        }
+        msg = String.format("学生%s的分数是%d，评级是%s", student, score, level);
+
+        return msg;
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
