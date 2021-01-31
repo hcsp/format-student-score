8e64eceabd8f1bb1c9164110d5bcdd525adee054
haileeliang <haileeliang62@gmail.com>
2021-01-31 15:01:14 +0900

    Revert "学生分数(修改)"
    This reverts commit 62bbb11c

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.calculation;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     /**
      * 输入学生姓名和分数，当： 90<=score<=100分时，返回"学生X的分数是Y，评级是A"；
@@ -11,16 +14,16 @@ public class Main {
      * <p>score为其他值时，返回"非法输入"。
      *
      * @param student 学生的姓名
-     * @param score   学生的分数
+     * @param score 学生的分数
      * @return 结果字符串
      */
     public static String formatStudentScore(String student, int score) {
-        if (score >= 90 && score <= 100) {
-            return "学生" + student + "分数是" + score + ",评级是A";
-        } else if (score >= 60 && score <= 89) {
-            return "学生" + student + "分数是" + score + ",评级是B";
-        } else if (score >= 0 && score <= 59) {
-            return "学生" + student + "分数是" + score + ",评级是C";
+        if ( score >= 90 && score <=100 ) {
+            return "学生"+ student + "分数是" + score + ",评级是A";
+        } else if ( score >= 60 && score <= 89) {
+            return "学生"+ student + "分数是" + score + ",评级是B";
+        } else if ( score >= 0 && score <= 59) {
+            return "学生"+ student + "分数是" + score + ",评级是C";
         } else {
             return "非法输入";
         }
