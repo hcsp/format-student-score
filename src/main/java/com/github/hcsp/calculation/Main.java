065a727c2211a20a6f8fdb192624cc39d97a4600
Bowen Wu <bwwu@iflytek.com>
2021-06-01 13:57:09 +0800

    feat: complete string connection

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -11,10 +11,24 @@ public class Main {
      * <p>score为其他值时，返回"非法输入"。
      *
      * @param student 学生的姓名
-     * @param score 学生的分数
+     * @param score   学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        String grade = "";
+        if (score >= 0 && score <= 100) {
+            if (score >= 90) {
+                grade = "A";
+            } else if (score >= 60) {
+                grade = "B";
+            } else {
+                grade = "C";
+            }
+            return "学生" + student + "的分数是" + score + "，评级是" + grade;
+        } else {
+            return "非法输入";
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
