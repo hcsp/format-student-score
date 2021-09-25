1fc6aa79997eb8c48ffad57ecc54dd2ff59ad455
DEMAO <987854018@qq.com>
2021-09-26 07:37:32 +0800

    Revert "练习String的加法操作 订正"
    This reverts commit 1580580ddd4e3c70c0a50fbf30ec3d3d46566eb9.

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -16,13 +16,13 @@ public class Main {
      */
     public static String formatStudentScore(String student, int score) {
         if (score >= 90 && score <=100) {
-            return "学生" + student + "的分数是" + score + "，评级是A";
-        } else if (score >= 60 && score <= 89) {
-            return "学生" + student + "的分数是" + score + ",评级是B";
-        } else if (score >= 0 && score <= 59) {
-            return "学生" + student + "的分数是" + score + ",评级是D";
+            return ("学生" + student + "的分数是" + score + "，评级是A");
+        } else if (score >= 60 && score <89) {
+            return ("学生" + student + "的分数是" + score + ",评级是B");
+        } else if (score >= 0 && score <59) {
+            return ("学生" + student + "的分数是" + score + ",评级是D");
         } else {
-            return "非法输入";
+            return ("非法输入");
         }
     }
 
