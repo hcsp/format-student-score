e448eab167d6d499dd7acdb005bc226826b3205c
DEMAO <987854018@qq.com>
2021-09-26 07:44:07 +0800

    练习String的加法操作 订正

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -15,12 +15,12 @@ public class Main {
      * @return 结果字符串
      */
     public static String formatStudentScore(String student, int score) {
-        if (score >= 90 && score <=100) {
+        if (score >= 90 && score <= 100) {
             return "学生" + student + "的分数是" + score + "，评级是A";
         } else if (score >= 60 && score <= 89) {
             return "学生" + student + "的分数是" + score + ",评级是B";
         } else if (score >= 0 && score <= 59) {
-            return "学生" + student + "的分数是" + score + ",评级是D";
+            return "学生" + student + "的分数是" + score + ",评级是C";
         } else {
             return "非法输入";
         }
