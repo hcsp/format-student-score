1f234a51bcfd0076cd6ac223052c60323ff8dd4a
JerryWang <1976883731@qq.com>
2021-10-31 01:05:11 +0800

    Fix bug

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -16,11 +16,11 @@ public class Main {
      */
     public static String formatStudentScore(String student, int score) {
         if (score >= 90 && score <= 100) {
-            return "学生" + student + "的分数是" + score + ",平级是A";
+            return "学生" + student + "的分数是" + score + ",评级是A";
         } else if (score >= 60 && score <= 89) {
-            return "学生" + student + "的分数是" + score + ",平级是B";
+            return "学生" + student + "的分数是" + score + ",评级是B";
         } else if (score >= 0 && score <= 59) {
-            return "学生" + student + "的分数是" + score + ",平级是C";
+            return "学生" + student + "的分数是" + score + ",评级是C";
         }
         return "非法输入";
     }
