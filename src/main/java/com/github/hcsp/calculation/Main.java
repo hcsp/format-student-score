9be0a5ec8ae962c2d3b24d2f141e04ce734bb298
赵童 <freetie@qq.com>
2022-02-16 13:42:59 +0800

    完成formatStudentScore方法 (#270)
    完成formatStudentScore方法 (#270)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -14,7 +14,20 @@ public class Main {
      * @param score 学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        String rating;
+        if (score > 100 || score < 0) {
+            return "非法输入";
+        }
+        if (score >= 90) {
+            rating = "A";
+        } else if (score >= 60) {
+            rating = "B";
+        } else {
+            rating = "C";
+        }
+        return "学生" + student + "的分数是" + score + "，评级是" + rating;
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
