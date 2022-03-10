eba9206bc32f97b151475c371a855a55b5e11414
Allen Yan <yanlezhong@gmail.com>
2022-03-10 22:28:12 +0800

    feat: format student score (#271)
    feat: format student score (#271)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -14,7 +14,20 @@ public class Main {
      * @param score 学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        String rating = "";
+        if (score >= 90 && score <=100) {
+            rating = "A";
+        } else if (score >= 60 && score <= 89) {
+            rating = "B";
+        } else if (score >= 0 && score <= 59) {
+            rating = "C";
+        }
+        if (rating.equals("")) {
+            return "非法输入";
+        }
+        return "学生" + student + "的分数是" + score + "，评级是" + rating;
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
