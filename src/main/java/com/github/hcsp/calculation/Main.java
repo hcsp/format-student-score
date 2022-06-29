5a118503276bf3cd441db4e36546a7a38b097e32
陈祥 <indexchen@foxmail.com>
2022-06-29 22:29:30 +0800

    format student score2

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -18,6 +18,9 @@ public class Main {
         if (score >= 60 && score <= 89) {
             return "学生" + student + "的分数是" + score + "评级是B";
         }
+        if (score >= 90 && score <= 100) {
+            return "学生" + student + "的分数是" + score + "评级是A";
+        }
         if (score >= 0 && score <= 59) {
             return "学生" + student + "的分数是" + score + "评级是C";
         }
