560441acd8c477504b7d148b42ce61680b8d8b30
spearhead0428 <hastur1016@gmail.com>
2023-05-22 08:49:46 +0800

    1232132123

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -14,7 +14,19 @@ public class Main {
      * @param score 学生的分数
      * @return 结果字符串
      */
-    public static String formatStudentScore(String student, int score) {}
+    public static String formatStudentScore(String student, int score) {
+        String Student_score;
+        if(score>= 60 && score <= 89){
+            Student_score = "B";
+        }else if (score >= 0 && score <=59){
+            Student_score = "C";
+        }else if (score >= 90 && score <= 100){
+            Student_score = "A";
+        }else{
+            Student_score = "非法输入";
+        }
+        return Student_score;
+    }
 
     public static void main(String[] args) {
         System.out.println(formatStudentScore("张三", -1));
